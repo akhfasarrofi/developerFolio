7cf44c449f2611f4867d0e3cf86b9944f7025ced
dopassss <zakaria.akhfas@gmail.com>
2024-09-03 16:02:16 +0700

    update

--- src/portfolio.js
+++ src/portfolio.js
@@ -20,26 +20,24 @@ const illustration = {
 };
 
 const greeting = {
-  username: "Saad Pasta",
-  title: "Hi all, I'm Saad",
+  username: "Zakaria Akhfas Arrofi",
+  title: "Hi all, I'm Akhfas",
   subTitle: emoji(
-    "A passionate Full Stack Software Developer 🚀 having an experience of building Web and Mobile applications with JavaScript / Reactjs / Nodejs / React Native and some other cool libraries and frameworks."
+    "A passionate Full Stack Software Developer 🚀 having an experience of building Web applications with JavaScript or Typescript / Nextjs, Vuejs / Nodejs and some other cool libraries and frameworks. Motivated and skilled Frontend Web Developer with a proven track record of creating visual appealing and user-friendly websites. Proficient Typescript and modern frontend libraries/frameworks. Committed to delivering high-cuality and collaborating effectively with cross-functional teams. Eager to leverage expertise and creativity to contribute to meaningful projects and drive innovation"
   ),
   resumeLink:
-    "https://drive.google.com/file/d/1ofFdKF_mqscH8WvXkSObnVvC9kK7Ldlu/view?usp=sharing", // Set to empty to hide the button
+    "https://drive.google.com/file/d/1xx_buXkYqWrN1iPJlcCdeGBtJgia2egE/view?usp=sharing", // Set to empty to hide the button
   displayGreeting: true // Set false to hide this section, defaults to true
 };
 
 // Social Media Links
 
 const socialMediaLinks = {
-  github: "https://github.com/saadpasta",
-  linkedin: "https://www.linkedin.com/in/saadpasta/",
-  gmail: "saadpasta70@gmail.com",
-  gitlab: "https://gitlab.com/saadpasta",
-  facebook: "https://www.facebook.com/saad.pasta7",
-  medium: "https://medium.com/@saadpasta",
-  stackoverflow: "https://stackoverflow.com/users/10422806/saad-pasta",
+  github: "https://github.com/akhfasarrofi",
+  linkedin: "https://www.linkedin.com/in/dopassss/",
+  gmail: "zakaria_akhfas@outlook.co.id",
+  medium: "https://medium.com/@zakaria.akhfas",
+  stackoverflow: "https://stackoverflow.com/users/11994763/zakaria-akhfas",
   // Instagram, Twitter and Kaggle are also supported in the links!
   // To customize icons and social links, tweak src/components/SocialMedia
   display: true // Set true to display this section, defaults to false
@@ -117,7 +115,7 @@ https://fontawesome.com/icons?d=gallery */
       fontAwesomeClassname: "fab fa-docker"
     }
   ],
-  display: true // Set false to hide this section, defaults to true
+  display: false // Set false to hide this section, defaults to true
 };
 
 // Education Section
@@ -126,24 +124,11 @@ const educationInfo = {
   display: true, // Set false to hide this section, defaults to true
   schools: [
     {
-      schoolName: "Harvard University",
-      logo: require("./assets/images/harvardLogo.png"),
-      subHeader: "Master of Science in Computer Science",
-      duration: "September 2017 - April 2019",
-      desc: "Participated in the research of XXX and published 3 papers.",
-      descBullets: [
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit"
-      ]
+      schoolName: "Yogyakarta Technology University",
+      logo: require("./assets/images/uty.png"),
+      subHeader: "Bachelor of Informatics",
+      duration: "September 2015 - April 2020",
     },
-    {
-      schoolName: "Stanford University",
-      logo: require("./assets/images/stanfordLogo.png"),
-      subHeader: "Bachelor of Science in Computer Science",
-      duration: "September 2013 - April 2017",
-      desc: "Ranked top 10% in the program. Took courses about Software Engineering, Web Security, Operating Systems, ...",
-      descBullets: ["Lorem ipsum dolor sit amet, consectetur adipiscing elit"]
-    }
   ]
 };
 
@@ -153,16 +138,16 @@ const techStack = {
   viewSkillBars: true, //Set it to true to show Proficiency Section
   experience: [
     {
-      Stack: "Frontend/Design", //Insert stack or technology you have experience in
+      Stack: "Frontend", //Insert stack or technology you have experience in
       progressPercentage: "90%" //Insert relative proficiency in percentage
     },
     {
       Stack: "Backend",
-      progressPercentage: "70%"
+      progressPercentage: "60%"
     },
     {
       Stack: "Programming",
-      progressPercentage: "60%"
+      progressPercentage: "80%"
     }
   ],
   displayCodersrank: false // Set true to display codersrank badges section need to changes your username in src/containers/skillProgress/skillProgress.js:17:62, defaults to false
@@ -174,29 +159,65 @@ const workExperiences = {
   display: true, //Set it to true to show workExperiences Section
   experience: [
     {
-      role: "Software Engineer",
-      company: "Facebook",
-      companylogo: require("./assets/images/facebookLogo.png"),
-      date: "June 2018 – Present",
-      desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+      role: "Senior Frontend Engineer",
+      company: "Komerce",
+      companylogo: require("./assets/images/komerce.png"),
+      date: "Mei 2022 – Present",
+      desc: "A leading tech-startup company in Central Java. Advancing SMEs by becoming an end-to-end e-commerce enabler for them. As a Frontend Web Developer, i have responsibilities as below",
       descBullets: [
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit"
+        "Optimized website performance by reducing the number of requests and increasing page speed.",
+        "Integrated a payment system that allowed customers to easily pay for products or services online.",
+        "Created a user authentication system that improved security and user privacy.",
+        "Utilized HTMLX, CSSX, and JavaScript to create a dynamic, interactive user interface that improved user experience."
       ]
     },
     {
       role: "Front-End Developer",
-      company: "Quora",
-      companylogo: require("./assets/images/quoraLogo.png"),
-      date: "May 2017 – May 2018",
-      desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+      company: "Agri Sparta Indonesia",
+      companylogo: require("./assets/images/agrisparta.jpeg"),
+      date: "October 2023 – March 2024",
+      desc: "Revolutionizes Indonesian farming with seed biotechnology and AI crop monitoring, boosting rice yields, enhancing crop management, and promoting sustainable agricultural practices for a better future.",
+      descBullets: [
+        "Refactored existing code to improve readability and maintainability, resulting in a X% reduction in development time.",
+        "Coordinated office events and meetings, resulting in a successful team-building environment.",
+        "Developed a full stack web application using modern technologies such as React and Supabase.",
+        "Utilized React to develop reusable components and optimize performance."
+      ]
     },
     {
-      role: "Software Engineer Intern",
-      company: "Airbnb",
-      companylogo: require("./assets/images/airbnbLogo.png"),
-      date: "Jan 2015 – Sep 2015",
-      desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+      role: "Front-End Developer",
+      company: "Komoverse",
+      companylogo: require("./assets/images/komoverse.jpeg"),
+      date: "January 2023 – September 2023",
+      desc: "As one of the very first NFT-driven gaming platforms built on the Solana blockchain.",
+      descBullets: [
+        "Building v2 komoverse.io",
+        "Developed and integrated blockchain applications with Solana extension for seamless connectivity and security.",
+        "Implemented Solana extension in blockchain apps, enhancing connectivity and decentralized operations."
+      ]
+    },
+    {
+      role: "Front-End Developer",
+      company: "Koding Works",
+      companylogo: require("./assets/images/kodingworks.png"),
+      date: "May 2022 – August 2022",
+      desc: "KodingWorks is a technology development platform.",
+      descBullets: [
+        "Conducted troubleshooting and routine maintenance.",
+        "Integrated blockchain crypto with the payment gateway system.",
+        "Maintenance single page application hotels dafam.",
+      ]
+    },
+    {
+      role: "Front-End Developer",
+      company: "Aksaramaya",
+      companylogo: require("./assets/images/aksaramaya.png"),
+      date: "November 2020 – May 2022",
+      desc: "Technology Platform Developer to Form National Digital Content Ecosystem.",
+      descBullets: [
+        "Develop applications based on the requirements given from the project manager",
+        "Develop application school digital library."
+      ]
     }
   ]
 };
@@ -216,28 +237,72 @@ const bigProjects = {
   subtitle: "SOME STARTUPS AND COMPANIES THAT I HELPED TO CREATE THEIR TECH",
   projects: [
     {
-      image: require("./assets/images/saayaHealthLogo.webp"),
-      projectName: "Saayahealth",
-      projectDesc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+      image: require("./assets/images/komads.png"),
+      projectName: "Komads",
+      projectDesc: "Platform dashboard to make it easier for online business people to advertise products",
       footerLink: [
         {
           name: "Visit Website",
-          url: "http://saayahealth.com/"
+          url: "https://komads.id"
         }
         //  you can add extra buttons here.
       ]
     },
     {
-      image: require("./assets/images/nextuLogo.webp"),
-      projectName: "Nextu",
-      projectDesc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+      image: require("./assets/images/komcards.png"),
+      projectName: "Komcards",
+      projectDesc: "Dashboard virtual debit card for online business",
       footerLink: [
         {
           name: "Visit Website",
-          url: "http://nextu.se/"
+          url: "https://komcards.id"
         }
       ]
-    }
+    },
+    {
+      image: require("./assets/images/komship.png"),
+      projectName: "Komship",
+      projectDesc: "Shipping platform",
+      footerLink: [
+        {
+          name: "Visit Website",
+          url: "https://komship.id"
+        }
+      ]
+    },
+    {
+      image: require("./assets/images/komoverse.jpeg"),
+      projectName: "Komo Game Hub",
+      projectDesc: "As one of the very first NFT-driven gaming platforms built on the Solana blockchain.",
+      footerLink: [
+        {
+          name: "Visit Website",
+          url: "https://komoverse.io"
+        }
+      ]
+    },
+    {
+      image: require("./assets/images/pesanio.jpeg"),
+      projectName: "Hotel Dafam",
+      projectDesc: "Mobile web application for booking hotel",
+      footerLink: [
+        {
+          name: "Visit Website",
+          url: "https://m.haidafam.com"
+        }
+      ]
+    },
+    {
+      image: require("./assets/images/edoo.png"),
+      projectName: "Edoo",
+      projectDesc: "Digital School Library",
+      footerLink: [
+        {
+          name: "Visit Website",
+          url: "https://edoo.id"
+        }
+      ]
+    },
   ],
   display: true // Set false to hide this section, defaults to true
 };
@@ -252,7 +317,7 @@ const achievementSection = {
 
   achievementsCards: [
     {
-      title: "Google Code-In Finalist",
+      title: "CCNA",
       subtitle:
         "First Pakistani to be selected as Google Code-in Finalist from 4000 students from 77 different countries.",
       image: require("./assets/images/codeInLogo.webp"),
@@ -300,7 +365,7 @@ const achievementSection = {
       ]
     }
   ],
-  display: true // Set false to hide this section, defaults to true
+  display: false // Set false to hide this section, defaults to true
 };
 
 // Blogs Section
@@ -343,7 +408,7 @@ const talkSection = {
       event_url: "https://www.facebook.com/events/2339906106275053/"
     }
   ],
-  display: true // Set false to hide this section, defaults to true
+  display: false // Set false to hide this section, defaults to true
 };
 
 // Podcast Section
@@ -356,7 +421,7 @@ const podcastSection = {
   podcast: [
     "https://anchor.fm/codevcast/embed/episodes/DevStory---Saad-Pasta-from-Karachi--Pakistan-e9givv/a-a15itvo"
   ],
-  display: true // Set false to hide this section, defaults to true
+  display: false // Set false to hide this section, defaults to true
 };
 
 // Resume Section
@@ -379,11 +444,11 @@ const contactInfo = {
 // Twitter Section
 
 const twitterDetails = {
-  userName: "twitter", //Replace "twitter" with your twitter username without @
+  userName: "dopasss", //Replace "twitter" with your twitter username without @
   display: true // Set true to display this section, defaults to false
 };
 
-const isHireable = false; // Set false if you are not looking for a job. Also isHireable will be display as Open for opportunities: Yes/No in the GitHub footer
+const isHireable = true; // Set false if you are not looking for a job. Also isHireable will be display as Open for opportunities: Yes/No in the GitHub footer
 
 export {
   illustration,
